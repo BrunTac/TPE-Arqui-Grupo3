4c07363950706524fc55578740a44cb8009ffc28
isa-conde <isconde@itba.edu.ar>
2024-10-22 14:23:21 -0300

    Cambios para poder pushear todo junto

---
+++ Kernel/include/videoDriver.h
@@ -0,0 +1,9 @@
+#ifndef VIDEO_DRIVER_H
+#define VIDEO_DRIVER_H
+
+#include <stdint.h>
+
+void putPixel(uint32_t hexColor, uint64_t x, uint64_t y);
+void drawchar(unsigned char c, int x, int y, int fgcolor, int bgcolor);
+
+#endif
