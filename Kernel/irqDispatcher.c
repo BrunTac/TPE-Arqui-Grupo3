0ffcf4303047da30048d9c29292ececd3e12dc4e
isa-conde <isconde@itba.edu.ar>
2024-10-25 16:28:12 -0300

    Recuperando

--- Kernel/irqDispatcher.c
+++ Kernel/irqDispatcher.c
@@ -1,20 +1,12 @@
 #include <time.h>
 #include <stdint.h>
 
-
-extern uint8_t keyboard_read();
 static void int_20();
-static void int_16();
 
 void irqDispatcher(uint64_t irq) {
-	
-	
 	switch (irq) {
 		case 0:
 			int_20();
-			break;
-		case 1:
-			int_16();
 			break;
 	}
 	return;
@@ -23,11 +15,3 @@ void irqDispatcher(uint64_t irq) {
 void int_20() {
 	timer_handler();
 }
-
-void int_16() {
-	uint8_t scancode = keyboard_read();
-	char key = 
-}
-
-
-
