67e34dff54fc6c8b58b920dc6fbfce273e516baa
ToniVarettoni <tvarettoni@itba.edu.ar>
2024-10-31 14:38:30 -0300

    snakeando

--- Userland/SampleCodeModule/snake.c
+++ Userland/SampleCodeModule/snake.c
@@ -2,10 +2,12 @@
 #include <snake.h>
 #include <sys_calls.h>
 #include <libc.h>
+#include <stdlib.h>
+
 
 #define BLOCKSIZE 32
-    
 
+static Coordinates apple;
 
 void snake(){
 
@@ -15,47 +17,50 @@ void snake(){
     drawMap();
 
     Coordinates body1[100];
-    Player player1 = spawnPlayer(BLOCKSIZE, BLOCKSIZE, body1);
+    Player * player1; 
+    spawnPlayer(BLOCKSIZE, BLOCKSIZE, body1, player1);
 
+    spawnApple();
     if (cantPlayers == 2)
     {
         Coordinates body2[100];
-        Player player2 = spawnPlayer(BLOCKSIZE, sys_scrHeight() - BLOCKSIZE, body2);
+        Player * player2;
+        spawnPlayer(BLOCKSIZE, sys_scrHeight() - BLOCKSIZE, body2, player2);
     }
 
     int lost = 0;
 
     while (!lost)
     {
-        for(int i = 0; i < 100; i++){}
+        for(int i = 0; i < 1000000; i++){}
         //sys_sleep();
         if (canMove(player1))
         {
-            movePlayer(&player1);
-            updateDirection(&player1);
-
-        }else{
-            lost = 1;
+            updateDirection(player1);
+            lost = movePlayer(player1);
+            
+            
         }
-        
     }
+    clear();
 }
 
-Player spawnPlayer(int x, int y, Coordinates body[100]){
+void spawnPlayer(int x, int y, Coordinates body[100], Player * player){
     Coordinates aux1 = {x, y};
-    body[0] = aux1;
+    player->body[0] = aux1;
     Coordinates aux2 = {x + BLOCKSIZE, y};
-    body[1] = aux2;
+    player->body[1] = aux2;
     Coordinates aux3 = {x + 2*BLOCKSIZE, y};
-    body[2] = aux3;
-    Player player = {2, 0, 3, body, RIGHT};
+    player->body[2] = aux3;
+    player->head = 2;
+    player->tail = 0;
+    player->size = 3;
 
-    for (int i = 0; i < player.size; i++)
+    for (int i = 0; i < player->size; i++)
     {
         sys_drawSquare(RED, x + i*BLOCKSIZE, y);
     }
 
-    return player;
 }
 
 int menuSnake(){
@@ -87,41 +92,32 @@ void drawMap(){
 
 void updateDirection(Player * player){
 
-    char c = getChar();
+    char c = getCharSnake();
 
     switch (c)
     {
     case 'w':
-        player->currentDirection = UP;
+        if (player->currentDirection != DOWN)
+            player->currentDirection = UP;
         break;
     case 'a':
-        player->currentDirection = LEFT;
+        if (player->currentDirection != RIGHT)    
+            player->currentDirection = LEFT;
         break;
     case 'd':
-        player->currentDirection = RIGHT;
+        if (player->currentDirection != LEFT)
+            player->currentDirection = RIGHT;
         break;
     case 's':
-        player->currentDirection = DOWN;
+        if (player->currentDirection != UP)    
+            player->currentDirection = DOWN;
         break;
     }
 }
 
-void movePlayer(Player * player){
+int movePlayer(Player * player){
     
-    int tailX = player->body[player->tail].x;
-    int tailY = player->body[player->tail].y;
-    if (((tailX + tailY) % 2 == 0))
-    {
-        sys_drawSquare(DARK_GREEN, tailX, tailY);
-    }else{
-        sys_drawSquare(GREEN, tailX, tailY);
-    }
-    
-    if(player->tail == 100 - 1){
-        player->tail = 0;
-    }else{
-        player->tail += 1;
-    }
+    Coordinates prev = player->body[player->head];
 
     if(player->head == 100 - 1){
         player->head = 0;
@@ -129,7 +125,7 @@ void movePlayer(Player * player){
         player->head += 1;
     }
 
-    Coordinates prev = player->body[player->head];
+
 
     switch (player->currentDirection)
     {
@@ -150,9 +146,83 @@ void movePlayer(Player * player){
         player->body[player->head] = aux3;
         break;
     }
+    
+    int Collision = checkCollision(player);
+
+
+
+    if (Collision == 0){
+        int tailX = player->body[player->tail].x;
+        int tailY = player->body[player->tail].y;
+        if (((tailX + tailY) % 64 == 0))
+        {
+            sys_drawSquare(DARK_GREEN, tailX, tailY);
+        }else{
+            sys_drawSquare(GREEN, tailX, tailY);
+        }
+    
+        if(player->tail == 100 - 1){
+           player->tail = 0;
+        }else{
+            player->tail += 1;
+        }
+        
+    }else if (Collision == -1){
+        return 1;
+    }else if (Collision == 1)
+    {
+        if (((apple.x + apple.y) % 64 == 0))
+        {
+            sys_drawSquare(DARK_GREEN, apple.x, apple.y);
+        }else{
+            sys_drawSquare(GREEN, apple.x, apple.y);
+        }
+        spawnApple();
+    }
+    
+    
+   
     sys_drawSquare(RED, player->body[player->head].x, player->body[player->head].y);
+    return 0;
 }
 
-int canMove(Player player){
+int canMove(Player * player){
     return 1;
+}
+
+int checkCollision(Player * player){
+
+    if (player->body[player->head].x == apple.x &&
+            player->body[player->head].y == apple.y)
+    {
+        return 1;
+    }
+    
+
+    if (player->body[player->head].x >= 1024 || player->body[player->head].y >= 1024
+        || player->body[player->head].x < 0 || player->body[player->tail].y < 0){
+        return -1;
+    }
+
+    int i = player->tail;
+    while (i != player->head)
+    {
+        if (player->body[player->head].x == player->body[i].x &&
+            player->body[player->head].y == player->body[i].y){
+            return -1;
+        }
+
+        if (i == 99){
+            i = 0;
+        }else{
+            i++;
+        }
+    }
+    return 0;
+}
+
+void spawnApple(){
+    apple.x = 23 * BLOCKSIZE;
+    apple.y = 5 * BLOCKSIZE;
+    sys_drawSquare(BLUE, apple.x, apple.y);
 }
