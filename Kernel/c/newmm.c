6917b2a87b8f2457dc3ade052a72c5037218655d
BrunTac <btaccone@itba.edu.ar>
2025-06-01 21:28:49 -0300

    "working" memory manager

--- Kernel/c/newmm.c
+++ Kernel/c/newmm.c
@@ -2,27 +2,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define SIZEOF_HEADER sizeof(header_t)
-
-typedef char ALIGN[SIZEOF_HEADER];
-
-typedef union header {
-    typedef struct {
-        union header *next;
-        size_t size;
-        int isFree;
-    } s;
-    ALIGN align; 
-} header_t ;
-
-typedef struct {
-    header_t *firstBlock;
-    uintptr_t memStart;
-    uintptr_t memEnd;
-} mm_t ;
-
 mm_t *createMemoryManager(void * manager, void *memoryRegion, size_t regionSize) {
-    if(!memoryRegion || !memoryManager || regionSize < sizeof(header_t)) {
+    if(!memoryRegion || !manager || regionSize < sizeof(header_t)) {
         return NULL; // params
     }
 
@@ -40,13 +21,13 @@ mm_t *createMemoryManager(void * manager, void *memoryRegion, size_t regionSize)
     return memoryManager;
 }
 
-void *malloc(mm_t *mgr, size_t size) {
+void *malloc_mm(mm_t *mgr, size_t size) {
     if(!mgr || !size) {
-        return NULL; // params
+        return NULL; // param check
     }
 
     header_t *current = mgr->firstBlock;
-    while(1) {
+    while(current) {
         uintptr_t realSize = size + sizeof(header_t);
         uintptr_t newAllocEnd = (uintptr_t) current + realSize;
         uintptr_t boundary = (current->s.next ? (uintptr_t) current->s.next : mgr->memEnd);
@@ -68,9 +49,26 @@ void *malloc(mm_t *mgr, size_t size) {
                     return (void *) ((uintptr_t) current + sizeof(header_t)); // success, pero no crea un nuevo header 'next' ya que no hay lugar
             }
         }   
-        if(!current->s.next) {
-            return NULL; // no encontro suficiente espacio
+        current = current->s.next;
+    }
+    return NULL; // llego a null y no encontro espacio
+}
+
+void free_mm(mm_t *mgr, void *memToFree) {
+    if(!memToFree || !mgr)
+        return; // params check
+
+    header_t *current = mgr->firstBlock;
+    while(current) {
+        if((uintptr_t) current == (uintptr_t) memToFree - sizeof(header_t)) {
+            current->s.isFree = 1;
+            for(header_t *next = current->s.next ; next && next->s.isFree ; next = next->s.next) {
+                current->s.next = next->s.next;
+                current->s.size += next->s.size + sizeof(header_t);
+            }
+            return; // success, y se asegura de tomar todos los bloques liberados contiguos en frente suyo
         }
         current = current->s.next;
     }
+    return; // no encontro el puntero al bloque pedido para liberar
 }
--- Kernel/include/newmm.h
+++ Kernel/include/newmm.h
@@ -1,10 +1,32 @@
-#ifndef MEMORY_MANAGER_H
-#define MEMORY_MANAGER_H
+#ifndef NEWMM_H
+#define NEWMM_H
 
-#include <stdlib.h>
+#include <stddef.h>
+#include <stdint.h>
 
-MemoryManagerADT createMemoryManager(void *const restrict memoryForMemoryManager, void *const restrict managedMemory);
+#define SIZEOF_HEADER sizeof(header_t)
 
-void *allocMemory(MemoryManagerADT const restrict memoryManager, const size_t memoryToAllocate);
+typedef char ALIGN[SIZEOF_HEADER];
+
+typedef union header {
+    struct {
+        union header *next;
+        size_t size;
+        int isFree;
+    } s;
+    ALIGN align; 
+} header_t ;
+
+typedef struct {
+    header_t *firstBlock;
+    uintptr_t memStart;
+    uintptr_t memEnd;
+} mm_t ;
+
+mm_t *createMemoryManager(void * manager, void *memoryRegion, size_t regionSize);
+
+void *malloc_mm(mm_t *mgr, size_t size);
+
+void free_mm(mm_t *mgr, void *memToFree);
 
 #endif
